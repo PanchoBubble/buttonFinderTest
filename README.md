ff4e6b519025804248e396cb04bc5f380ff84b7a
Juan De luca <juan.deluca@bubblebpm.com>
2019-08-22 17:22:42 -0300

    small fixes

---
+++ README.md
@@ -0,0 +1,3 @@
+How does it work?
+
+python button_finder.py <origin url> <target url>
--- button_finder.py
+++ button_finder.py
@@ -4,6 +4,7 @@ import sys
 
 class ButtonFinder:
     def __init__(self, origin_url, target_url):
+        self.search_id = 'make-everything-ok-button'
         self.origin_url = origin_url
         self.target_url = target_url
         self.session = requests.Session()
@@ -13,15 +14,21 @@ class ButtonFinder:
     
     def __get_origin_button(self, html):
         soup = bs(html, features="html.parser")
-        return soup.find(id='make-everything-ok-button')
+        return soup.find(id=self.search_id)
 
     def __get_target_site(self):
         return self.session.get(self.target_url).text
     
     def __get_target_button(self, html, origin_ok_button):
         soup = bs(html, features="html.parser")
-        for key in list(origin_ok_button.attrs.keys()):
-            target_button = soup.find('a', {key : origin_ok_button.attrs[key]})
+        constainer_div = soup.find('div', {'class':'panel-body'})
+        buttonAttrs = {
+            'id' : origin_ok_button.attrs['id'],
+            'class' : origin_ok_button.attrs['class'][1],
+            'text' : (origin_ok_button.get_text()).strip()
+        }
+        for key in list(buttonAttrs.keys()):
+            target_button = constainer_div.find('a', {key :buttonAttrs[key]})
             if target_button is not None:
                 return target_button
         else:
